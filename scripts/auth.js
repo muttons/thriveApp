535fadd47796aa5dc13bf285ad7d9bc110be8b15
muttons <55284268+muttons@users.noreply.github.com>
2020-03-04 09:10:58 -0500

    ...

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,9 +1,27 @@
 
+
 function signIn(){
-var provider = new firebase.auth.OAuthProvider('microsoft.com');
-provider.setCustomParameters({
-});
-firebase.auth().signInWithRedirect(provider)
+    var provider = new firebase.auth.OAuthProvider('microsoft.com');
+    provider.setCustomParameters({
+        tenant: 'common'
+    });
+    firebase.auth().signInWithRedirect(provider)
+      .then(function(result) {
+        // User is signed in.
+        // IdP data available in result.additionalUserInfo.profile.
+        // OAuth access token can also be retrieved:
+        // result.credential.accessToken
+        // OAuth ID token can also be retrieved:
+        // result.credential.idToken
+        console.log("Success");
+      })
+      .catch(function(error) {
+        // Handle error.
+        console.log("Failure");
+      });
+    }
+
+    firebase.auth().getRedirectResult()
   .then(function(result) {
     // User is signed in.
     // IdP data available in result.additionalUserInfo.profile.
@@ -11,20 +29,16 @@ firebase.auth().signInWithRedirect(provider)
     // result.credential.accessToken
     // OAuth ID token can also be retrieved:
     // result.credential.idToken
-    console.log("Success");
   })
   .catch(function(error) {
     // Handle error.
-    console.log("Failure");
   });
-}
 
-
-function signOut(){
-firebase.auth().signOut().then(function() {
-    // Sign-out successful.
-    console.log("Successfull Log Out")
-  }).catch(function(error) {
-    // An error happened.
-  });
-}
+    function signOut(){
+    firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+        console.log("Successfull Log Out")
+      }).catch(function(error) {
+        // An error happened.
+      });
+    }
