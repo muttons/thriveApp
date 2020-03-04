f97662686c604d346af2822f719581b5a073be31
muttons <55284268+muttons@users.noreply.github.com>
2020-03-04 08:42:52 -0500

    kinda working

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,65 +1,27 @@
 
-function signIn(){
+function microsoftSignIn(){
 var provider = new firebase.auth.OAuthProvider('microsoft.com');
-provider.setCustomParameters({
-    // Optional "tenant" parameter in case you are using an Azure AD tenant.
-    // eg. '8eaef023-2b34-4da1-9baa-8bc8c9d6a490' or 'contoso.onmicrosoft.com'
-    // or "common" for tenant-independent tokens.
-    // The default value is "common".
-    tenant: 'bc393147-bc67-4de1-b298-0a58f5b08534'
+firebase.auth().signInWithPopup(provider)
+  .then(function(result) {
+    // User is signed in.
+    // IdP data available in result.additionalUserInfo.profile.
+    // OAuth access token can also be retrieved:
+    // result.credential.accessToken
+    // OAuth ID token can also be retrieved:
+    // result.credential.idToken
+    console.log("Success");
+  })
+  .catch(function(error) {
+    // Handle error.
+    console.log("Failure");
   });
-
-  firebase.auth().signInWithRedirect(provider);
 }
 
 
-
-// listen for auth status changes
-auth.onAuthStateChanged(user => {
-    if (user) {
-      console.log('user logged in: ', user);
-    } else {
-      console.log('user logged out');
-    }
-  })
-  
-  // signup
-  const signupForm = document.querySelector('#signup-form');
-  signupForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    
-    // get user info
-    const email = signupForm['signup-email'].value;
-    const password = signupForm['signup-password'].value;
-  
-    // sign up the user
-    auth.createUserWithEmailAndPassword(email, password).then(cred => {
-      // close the signup modal & reset form
-      const modal = document.querySelector('#modal-signup');
-      M.Modal.getInstance(modal).close();
-      signupForm.reset();
-    });
+function microsoftSignOut(){
+firebase.auth().signOut().then(function() {
+    // Sign-out successful.
+  }).catch(function(error) {
+    // An error happened.
   });
-  
-  // logout
-  const logout = document.querySelector('#logout');
-  logout.addEventListener('click', (e) => {
-    e.preventDefault();
-    auth.signOut();
-  });
-  
-  // login
-  const loginForm = document.querySelector('#login-form');
-  loginForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    
-  
-    // log the user in
-   firebase.auth().signInWithRedirect(provider).then((cred) => {
-      // close the signup modal & reset form
-      const modal = document.querySelector('#modal-login');
-      M.Modal.getInstance(modal).close();
-      loginForm.reset();
-    });
-  
-  });
+}
