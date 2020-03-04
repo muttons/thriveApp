05575f3ed332504117b8bf0052e4cadbb324c1a6
muttons <55284268+muttons@users.noreply.github.com>
2020-03-04 14:04:13 -0500

    working login/out with content

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,22 +1,34 @@
-authProvider = new firebase.auth.OAuthProvider('microsoft.com');
 
-microsoftSignIn = () => {
-    firebase.auth().signInWithRedirect(authProvider).then(function(result){
-        console.log(result);
-        console.log("Successfull Sign In with Microsoft Account");
-    }).catch(function(err){
-        console.log(err);
-        console.log("Failed to Sign In with Microsoft Account");
-    });
-}
+// listen for auth status changes
+auth.onAuthStateChanged(user => {
+    if (user) {
+        // return data from firebase if the user is logged in
+        db.collection('guides').get().then(snapshot => {
+            setupGuides(snapshot.docs)
+        });
+    } else {
+        // return and empty array if they are logged out
+      setupGuides([]);
+}});
 
 
-microsoftSignOut = () => {
-    firebase.auth().signOut().then(function() {
-        // Sign-out successful.
-        console.log("Successfull Log Out")
-      }).catch(function(error) {
-        // An error happened.
-      });
+// Sign In with Microsoft OAuth
+const authProvider = new firebase.auth.OAuthProvider('microsoft.com');
+    microsoftSignIn = () => {
+        firebase.auth().signInWithRedirect(authProvider).then(function(result){
+            console.log(result);
+            console.log("Successfull Sign In with Microsoft Account");
+        }).catch(function(err){
+            console.log(err);
+            console.log("Failed to Sign In with Microsoft Account");
+        });
     }
 
+// Sign Out
+microsoftSignOut = () => {
+firebase.auth().signOut().then(function() {
+    // Sign-out successful.
+  }).catch(function(error) {
+    // An error happened.
+  });
+}
