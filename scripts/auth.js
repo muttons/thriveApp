42804e67af892f8a03b2a0e213193125b0203b71
muttons <55284268+muttons@users.noreply.github.com>
2020-03-05 13:34:09 -0500

    functions added

--- scripts/auth.js
+++ scripts/auth.js
@@ -40,18 +40,13 @@ createForm.addEventListener('submit', (e) => {
 // Sign In with Microsoft OAuth
 const authProvider = new firebase.auth.OAuthProvider('microsoft.com');
     microsoftSignIn = () => {
-
       authProvider.setCustomParameters({
         // Have user select account - bypassess seamless sign on for the app
         prompt: 'select_account'
       });
-
         firebase.auth().signInWithRedirect(authProvider).then(function(result){
             console.log(result);
             console.log("Successfull Sign In with Microsoft Account");
-        }).catch(function(err){
-            console.log(err);
-            console.log("Failed to Sign In with Microsoft Account");
         });
     }
 
