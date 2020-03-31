a1d94dc216bf346a3620de8469293f35d8869e9d
muttons <55284268+muttons@users.noreply.github.com>
2020-03-31 09:40:04 -0400

    working ticket submission and file upload

--- scripts/email.js
+++ scripts/email.js
@@ -1,9 +1,9 @@
-// getTestOne
-const testForm = document.querySelector('#test-form-one');
+// getticketOne
+const ticketForm = document.querySelector('#ticket-form-one');
 
 
 firebase.auth().onAuthStateChanged(function(user) {
-testForm.addEventListener('submit', (e) => {
+ticketForm.addEventListener('submit', (e) => {
   e.preventDefault();
       //preloader
       document.querySelector('.loader1').classList.add('progress');
@@ -14,21 +14,28 @@ testForm.addEventListener('submit', (e) => {
       document.querySelector('.loader2').classList.remove('indeterminate');
     },3000);
 
-      //adds the data to the testOne collection
+      //adds the data to the mail collection
       db.collection('mail').add({
         
         from: user.email,
         to: 'help.desk@thriveupstate.org',
         replyTo: user.email,
         message: {
-        subject: testForm.subject.value,
-        text: testForm.text.value,
-        html: `<h2> New ticket from Thrive App Submitted by</h2> ${user.email}.` 
+        subject: ticketForm.subject.value,
+        text: ticketForm.text.value,
+        html: `
+        
+        <h2> New ticket from Thrive App Submitted by ${user.email} </h2> 
+        <br><br> 
+        <h3> Ticket Information </h3> 
+        <br>
+        ${ticketForm.text.value}
+        ` 
         }
 
       }).then(() => {
-        // close the test modal & reset form
-        const modal = document.querySelector('#modal-test');
+        // close the ticket modal & reset form
+        const modal = document.querySelector('#modal-ticket');
         M.Modal.getInstance(modal).close();
 
       }).catch(err => {
