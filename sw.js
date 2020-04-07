6f5a9a03eda0865e87770da819be295b7af05830
muttons <55284268+muttons@users.noreply.github.com>
2020-04-07 08:57:27 -0400

    .

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'site-static-v10.14'; //CHANGE THIS - name of your static cache
-const dynamicCacheName = 'site-dynamic-v10.14'; //CHANGE THIS - name of your dynamic cache
+const staticCacheName = 'site-static-v10.15'; //CHANGE THIS - name of your static cache
+const dynamicCacheName = 'site-dynamic-v10.15'; //CHANGE THIS - name of your dynamic cache
 const assets = [ //CHANGE THIS - list of assets to automatically get added to the static cache
   '',
   '/index.html',
