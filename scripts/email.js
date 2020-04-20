8640044900184de62e5133723b0a090db82a81e2
muttons <55284268+muttons@users.noreply.github.com>
2020-04-20 08:48:00 -0400

    removed old search button

--- scripts/email.js
+++ scripts/email.js
@@ -1,54 +1,41 @@
-// getticketOne
-const ticketForm = document.querySelector('#ticket-form-one');
+require('dotenv').config();
+
+"use strict";
+const nodemailer = require("nodemailer");
+
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
 
 
-firebase.auth().onAuthStateChanged(function(user) {
-ticketForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-      //preloader
-      document.querySelector('.loader1').classList.add('progress');
-      document.querySelector('.loader2').classList.add('indeterminate');
-
-    setTimeout(function() {
-      document.querySelector('.loader1').classList.remove('progress');
-      document.querySelector('.loader2').classList.remove('indeterminate');
-    },3000);
-
-      //adds the data to the mail collection
-      db.collection('mail').add({
-        
-        from: user.email,
-        to: 'help.desk@thriveupstate.org',
-        replyTo: user.email,
-        message: {
-        subject: ticketForm.subject.value,
-        text: ticketForm.text.value,
-        html: `
-        
-        <h2> New ticket from Thrive App Submitted by ${user.email} </h2> 
-        <br><br> 
-        <h3> Ticket Information </h3> 
-        <br>
-        ${ticketForm.text.value}
-        ` ,
-        attachments: [
-          {
-            content: attachment,
-            filename: ticketForm.subject.value,
-            type: "application/pdf",
-            disposition: "attachment"
-          }
-        ]
-        }
-
-      }).then(() => {
-        // close the ticket modal & reset form
-        const modal = document.querySelector('#modal-ticket');
-        M.Modal.getInstance(modal).close();
-
-      }).catch(err => {
-        console.log(err.message);
-      });
-  });
+  // create reusable transporter object using the default SMTP transport
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.sendgrid.net',
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+        user: process.env.API_KEY,
+        pass: process.env.API_PASS
+    }
 });
+
+  // send mail with defined transport object
+  let info =  await transporter.sendMail({
+    from: '"test account" <foo@example.com>', // sender address
+    to: "help.desk@thriveupstate.org", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>" // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+main().catch(console.error);
+
+
 
