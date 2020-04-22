f185c4f9a495ade692b26495c7d0fee02161931e
muttons <55284268+muttons@users.noreply.github.com>
2020-04-22 08:35:26 -0400

    nodemailer changes

--- scripts/email.js
+++ scripts/email.js
@@ -1,10 +1,8 @@
 reguire('dotenv').config()
 
-"use strict";
-const nodemailer = require("nodemailer");
 
-// async..await is not allowed in global scope, must use a wrapper
-async function testMailer() {
+const nodemailer = require("nodemailer");
+function testMailer() {
 
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
@@ -18,20 +16,12 @@ async function testMailer() {
   });
 
   // send mail with defined transport object
-  let info = await transporter.sendMail({
+  let info = transporter.sendMail({
     from: '"Fred Foo 👻" <foo@example.com>', // sender address
     to: "bar@example.com, baz@example.com", // list of receivers
     subject: "Hello ✔", // Subject line
     text: "Hello world?", // plain text body
     html: "<b>Hello world?</b>" // html body
   });
-
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 }
 
-testMailer().catch(console.error);
