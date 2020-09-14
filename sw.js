a60bff994bbf45094e53536926f7b65173c3cd51
muttons <55284268+muttons@users.noreply.github.com>
2020-09-14 19:29:53 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'site-static-v10.60'; //CHANGE THIS - name of your static cache
-const dynamicCacheName = 'site-dynamic-v10.60'; //CHANGE THIS - name of your dynamic cache
+const staticCacheName = 'site-static-v10.61'; //CHANGE THIS - name of your static cache
+const dynamicCacheName = 'site-dynamic-v10.61'; //CHANGE THIS - name of your dynamic cache
 const assets = [ //CHANGE THIS - list of assets to automatically get added to the static cache
   '',
   '/index.html',
@@ -73,4 +73,44 @@ self.addEventListener('fetch', evt => {
       })
     );
   }
+});
+
+
+
+
+
+// custom install prompt tests
+
+window.addEventListener('beforeinstallprompt', (event) => {
+  console.log('👍', 'beforeinstallprompt', event);
+  // Stash the event so it can be triggered later.
+  window.deferredPrompt = event;
+  // Remove the 'hidden' class from the install button container
+  divInstall.classList.toggle('hidden', false);
+});
+
+
+
+butInstall.addEventListener('click', () => {
+  console.log('👍', 'butInstall-clicked');
+  const promptEvent = window.deferredPrompt;
+  if (!promptEvent) {
+    // The deferred prompt isn't available.
+    return;
+  }
+  // Show the install prompt.
+  promptEvent.prompt();
+  // Log the result
+  promptEvent.userChoice.then((result) => {
+    console.log('👍', 'userChoice', result);
+    // Reset the deferred prompt variable, since
+    // prompt() can only be called once.
+    window.deferredPrompt = null;
+    // Hide the install button.
+    divInstall.classList.toggle('hidden', true);
+  });
+});
+
+window.addEventListener('appinstalled', (event) => {
+  console.log('👍', 'appinstalled', event);
 });
