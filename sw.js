a3ed6eeb0d2c2b7c6e9977478641eee14998a71b
muttons <55284268+muttons@users.noreply.github.com>
2020-09-17 10:53:00 -0400

    added classes page

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'site-static-v10.72'; //CHANGE THIS - name of your static cache
-const dynamicCacheName = 'site-dynamic-v10.72'; //CHANGE THIS - name of your dynamic cache
+const staticCacheName = 'site-static-v10.73'; //CHANGE THIS - name of your static cache
+const dynamicCacheName = 'site-dynamic-v10.73'; //CHANGE THIS - name of your dynamic cache
 const assets = [ //CHANGE THIS - list of assets to automatically get added to the static cache
   '',
   '/index.html',
