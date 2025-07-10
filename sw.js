efff5757eea762d702f0be67c6a4e2d28d513acd
muttons <55284268+muttons@users.noreply.github.com>
2025-07-10 13:33:02 -0400

    partsList test

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'site-static-v16.79';
-const dynamicCacheName = 'site-dynamic-v16.79';
+const staticCacheName = 'site-static-v16.80';
+const dynamicCacheName = 'site-dynamic-v16.80';
 const assets = [
   '',
   '/index.html',
