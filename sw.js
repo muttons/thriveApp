bb5b4be2d5a68d03ce900f691498cfafdbbf2604
muttons <55284268+muttons@users.noreply.github.com>
2025-07-10 14:38:43 -0400

    ..

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'site-static-v16.89';
-const dynamicCacheName = 'site-dynamic-v16.89';
+const staticCacheName = 'site-static-v16.91';
+const dynamicCacheName = 'site-dynamic-v16.91';
 const assets = [
   '',
   '/index.html',
